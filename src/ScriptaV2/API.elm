c599d383523a864df35811c7aaf2a66a546bb676
James Carlson <jxxcarlson@gmail.com>
2023-12-04 20:43:05 -0500

    Add missing parameter to function compile (update for new API)

--- src/ScriptaV2/API.elm
+++ src/ScriptaV2/API.elm
@@ -15,10 +15,10 @@ import ScriptaV2.Language exposing (Language)
 {-| -}
 compile : Language -> Int -> Int -> String -> List String -> List (Element MarkupMsg)
 compile lang width outerCount selectedId lines =
-    ScriptaV2.Compiler.compile lang width outerCount selectedId lines |> ScriptaV2.Compiler.view
+    ScriptaV2.Compiler.compile lang width outerCount selectedId lines |> ScriptaV2.Compiler.view width
 
 
 {-| -}
 compileString : Language -> Int -> String -> List (Element MarkupMsg)
 compileString lang width str =
-    ScriptaV2.Compiler.compile lang width 0 "---" (String.lines str) |> ScriptaV2.Compiler.view
+    ScriptaV2.Compiler.compile lang width 0 "---" (String.lines str) |> ScriptaV2.Compiler.view width
