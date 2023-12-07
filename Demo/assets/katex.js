9450de6d134b0583f49d48fc99b3d8442a05d7b3
James Carlson <jxxcarlson@gmail.com>
2023-12-07 11:01:06 -0500

    KaTeX experiment with mhchem

--- Demo/assets/katex.js
+++ Demo/assets/katex.js
@@ -1,26 +1,40 @@
-const init =  async function(app) {
+const init = async function(app) {
 
   console.log("I am starting elm-katex: init");
-  var katexJs = document.createElement('script')
-  katexJs.type = 'text/javascript'
-  katexJs.onload = initKatex
-  katexJs.src = "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"
+
+  var katexJs = document.createElement('script');
+  katexJs.type = 'text/javascript';
+  katexJs.onload = function() {
+    console.log("elm-katex: katex loading");
+    initKatex();
+    console.log("elm-katex: mhchem loading");
+    loadMhchem();
+  };
+  katexJs.src = "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js";
 
   document.head.appendChild(katexJs);
   console.log("elm-katex: I have appended katexJs to document.head");
+}
 
+function loadMhchem() {
+  var mChemJs = document.createElement('script');
+  mChemJs.type = 'text/javascript';
+  mChemJs.onload = function() {
+    console.log("elm-katex: mhchem loaded");
+  };
+  mChemJs.src = "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/mhchem.min.js";
+
+  document.head.appendChild(mChemJs);
+  console.log("elm-katex: I have appended mChemJs to document.head");
 }
 
 function initKatex() {
-
   console.log("elm-katex: initializing");
 
   class MathText extends HTMLElement {
-
-     constructor() {
-         // Always call super first in constructor
-         super();
-       }
+    constructor() {
+      super();
+    }
 
     connectedCallback() {
       this.attachShadow({mode: "open"});
@@ -31,13 +45,10 @@ function initKatex() {
         );
       let link = document.createElement('link');
       link.setAttribute('rel', 'stylesheet');
-      link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css');
+      link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.13.3/dist/katex.min.css');
       this.shadowRoot.appendChild(link);
-
     }
-
   }
 
-  customElements.define('math-text', MathText)
-
+  customElements.define('math-text', MathText);
 }
