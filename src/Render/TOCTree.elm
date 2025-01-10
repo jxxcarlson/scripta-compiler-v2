1b6405afbad3109f64dcd979f57257c75b49b7dc
James Carlson <jxxcarlson@gmail.com>
2025-01-10 10:57:22 -0500

    Add to Render.TOCTree: rawToc : Forest ExpressionBlock -> List (Tree ExpressionBlock)

--- src/Render/TOCTree.elm
+++ src/Render/TOCTree.elm
@@ -1,7 +1,19 @@
 module Render.TOCTree exposing (..)
 
--- import Generic.Language exposing (ExpressionBlock)
+import Generic.ASTTools
+import Generic.Forest exposing (Forest)
+import Generic.Language exposing (ExpressionBlock)
+import Library.Forest
+import List.Extra
+import RoseTree.Tree as T exposing (Tree)
 
 
-foo =
-    1
+rawToc : Forest ExpressionBlock -> List (Tree ExpressionBlock)
+rawToc ast =
+    Generic.ASTTools.tableOfContents 8 ast
+        |> Library.Forest.makeForest tocLevel
+
+
+tocLevel : ExpressionBlock -> Int
+tocLevel { args } =
+    List.Extra.getAt 0 args |> Maybe.andThen String.toInt |> Maybe.withDefault 1
