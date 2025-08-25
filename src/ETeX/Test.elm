05c6bc09fc5a2e4271fe7da3e63750df9b48885e
James Carlson <jxxcarlson@gmail.com>
2025-08-25 10:47:15 -0400

    Fix rendering of head and body:
    ETeX.Test: add functions p and q:
      - p : String -> List Generic.Language.PrimitiveBlock
      - q : String -> List Generic.Language.ExpressionBlock

--- src/ETeX/Test.elm
+++ src/ETeX/Test.elm
@@ -1,6 +1,29 @@
-module ETeX.Test exposing (evalStr, transform)
+module ETeX.Test exposing (evalStr, p, q, t, transform)
 
 import ETeX.Transform exposing (makeMacroDict)
+import Generic.Language
+import Generic.Pipeline
+import M.Expression
+import M.PrimitiveBlock
+
+
+p : String -> List Generic.Language.PrimitiveBlock
+p str =
+    M.PrimitiveBlock.parse "0" 0 (String.lines str)
+
+
+q : String -> List Generic.Language.ExpressionBlock
+q =
+    p >> List.map expressionBlockFromPrimitiveBlock
+
+
+t =
+    "[i foo [link NYT https://nytimes.com]]\n\n"
+
+
+expressionBlockFromPrimitiveBlock : Generic.Language.PrimitiveBlock -> Generic.Language.ExpressionBlock
+expressionBlockFromPrimitiveBlock =
+    Generic.Pipeline.toExpressionBlock M.Expression.parse
 
 
 transform str =
